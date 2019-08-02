1dfb5ba07e2f57dc433b10ba3a89ad776ea77fe9
Vladimir Zahradnik <vladimir@zahradnik.io>
2019-08-02 10:37:12 +0200

    Add unit tests for clicks

--- test/action_click.cpp
+++ test/action_click.cpp
@@ -12,35 +12,114 @@ unittest_setup() {
     state->reset();
 }
 
-//unittest(receive_on_click_event_after_default_click_ticks) {
-//    // press button
-//    state->digitalPin[INPUT_PIN] = LOW;
-//    testMock.getButton().tick();
-//
-//    // release button
-//    state->digitalPin[INPUT_PIN] = HIGH;
-//    state->micros = DEFAULT_CLICK_TICKS_MS * 1000 + 1;
-//    testMock.getButton().tick();
-//
-//    // validate click event
-//    assertEqual(true, testMock.didReceiveOnClickEvent());
-//}
-//
-//unittest(receive_on_click_event_after_custom_click_ticks) {
-//    constexpr static int CLICK_TICKS_MS = 300;
-//    testMock.getButton().setClickTicks(CLICK_TICKS_MS);
-//
-//    // press button
-//    state->digitalPin[INPUT_PIN] = LOW;
-//    testMock.getButton().tick();
-//
-//    // release button
-//    state->digitalPin[INPUT_PIN] = HIGH;
-//    state->micros = CLICK_TICKS_MS * 1000 + 1;
-//    testMock.getButton().tick();
-//
-//    // validate click event
-//    assertEqual(true, testMock.didReceiveOnClickEvent());
-//}
+unittest(do_not_receive_on_click_event_before_default_click_ticks) {
+        // press button
+        state->digitalPin[INPUT_PIN] = LOW;
+        testMock.getButton().tick();
+
+        // fire press event after debounce period elapses
+        state->micros = (DEFAULT_DEBOUNCE_TICKS_MS + 1) * 1000;
+        testMock.getButton().tick();
+
+        // release button
+        state->digitalPin[INPUT_PIN] = HIGH;
+        state->micros = DEFAULT_CLICK_TICKS_MS * 1000;
+        testMock.getButton().tick();
+
+        // refresh state machine to get click event
+        testMock.getButton().tick();
+
+        // validate click event
+        assertEqual(1, testMock.getPressEventsReceivedCount());
+        assertEqual(1, testMock.getReleaseEventsReceivedCount());
+        assertEqual(0, testMock.getClickEventsReceivedCount());
+        assertEqual(0, testMock.getDoubleClickEventsReceivedCount());
+        assertEqual(0, testMock.getLongPressStartEventsReceivedCount());
+        assertEqual(0, testMock.getLongPressEndEventsReceivedCount());
+}
+
+unittest(receive_on_click_event_after_default_click_ticks) {
+    // press button
+    state->digitalPin[INPUT_PIN] = LOW;
+    testMock.getButton().tick();
+
+    // fire press event after debounce period elapses
+    state->micros = (DEFAULT_DEBOUNCE_TICKS_MS + 1) * 1000;
+    testMock.getButton().tick();
+
+    // release button
+    state->digitalPin[INPUT_PIN] = HIGH;
+    state->micros = (DEFAULT_CLICK_TICKS_MS + 1) * 1000;
+    testMock.getButton().tick();
+
+    // refresh state machine to get click event
+    testMock.getButton().tick();
+
+    // validate click event
+    assertEqual(1, testMock.getPressEventsReceivedCount());
+    assertEqual(1, testMock.getReleaseEventsReceivedCount());
+    assertEqual(1, testMock.getClickEventsReceivedCount());
+    assertEqual(0, testMock.getDoubleClickEventsReceivedCount());
+    assertEqual(0, testMock.getLongPressStartEventsReceivedCount());
+    assertEqual(0, testMock.getLongPressEndEventsReceivedCount());
+}
+
+unittest(do_not_receive_on_click_event_before_custom_click_ticks) {
+    constexpr static int CLICK_TICKS_MS = 300;
+    testMock.getButton().setClickTicks(CLICK_TICKS_MS);
+
+    // press button
+    state->digitalPin[INPUT_PIN] = LOW;
+    testMock.getButton().tick();
+
+    // fire press event after debounce period elapses
+    state->micros = (DEFAULT_DEBOUNCE_TICKS_MS + 1) * 1000;
+    testMock.getButton().tick();
+
+    // release button
+    state->digitalPin[INPUT_PIN] = HIGH;
+    state->micros = CLICK_TICKS_MS * 1000;
+    testMock.getButton().tick();
+
+    // refresh state machine to get click event
+    testMock.getButton().tick();
+
+    // validate click event
+    assertEqual(1, testMock.getPressEventsReceivedCount());
+    assertEqual(1, testMock.getReleaseEventsReceivedCount());
+    assertEqual(0, testMock.getClickEventsReceivedCount());
+    assertEqual(0, testMock.getDoubleClickEventsReceivedCount());
+    assertEqual(0, testMock.getLongPressStartEventsReceivedCount());
+    assertEqual(0, testMock.getLongPressEndEventsReceivedCount());
+}
+
+unittest(receive_on_click_event_after_custom_click_ticks) {
+    constexpr static int CLICK_TICKS_MS = 300;
+    testMock.getButton().setClickTicks(CLICK_TICKS_MS);
+
+    // press button
+    state->digitalPin[INPUT_PIN] = LOW;
+    testMock.getButton().tick();
+
+    // fire press event after debounce period elapses
+    state->micros = (DEFAULT_DEBOUNCE_TICKS_MS + 1) * 1000;
+    testMock.getButton().tick();
+
+    // release button
+    state->digitalPin[INPUT_PIN] = HIGH;
+    state->micros = (CLICK_TICKS_MS + 1) * 1000;
+    testMock.getButton().tick();
+
+    // refresh state machine to get click event
+    testMock.getButton().tick();
+
+    // validate click event
+    assertEqual(1, testMock.getPressEventsReceivedCount());
+    assertEqual(1, testMock.getReleaseEventsReceivedCount());
+    assertEqual(1, testMock.getClickEventsReceivedCount());
+    assertEqual(0, testMock.getDoubleClickEventsReceivedCount());
+    assertEqual(0, testMock.getLongPressStartEventsReceivedCount());
+    assertEqual(0, testMock.getLongPressEndEventsReceivedCount());
+}
 
 unittest_main()
