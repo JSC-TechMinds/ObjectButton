e27b593ea811450501d9c018d428b4b80eca98cf
vzahradnik <vzahradnik@users.noreply.github.com>
2023-05-21 08:20:24 +0000

    deploy: 563e53923c1e53e49b18c941a9fac65c3ab8f270

--- classjsc_1_1DigitalButton.js
+++ classjsc_1_1DigitalButton.js
@@ -1,5 +1,7 @@
 var classjsc_1_1DigitalButton =
 [
     [ "DigitalButton", "classjsc_1_1DigitalButton.html#adcafee17dc16a550623a5fe0819d91ee", null ],
-    [ "getId", "classjsc_1_1DigitalButton.html#a79f430bc9737899463dedfd626566bdc", null ]
+    [ "getId", "classjsc_1_1DigitalButton.html#a79f430bc9737899463dedfd626566bdc", null ],
+    [ "invertInputLogic", "classjsc_1_1DigitalButton.html#a63fba8dfbe835852bb64959286008716", null ],
+    [ "isInputLogicInverted", "classjsc_1_1DigitalButton.html#ab8828d1cf82ce70eccae9105063dc9d6", null ]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,7 @@
 var searchData=
 [
-  ['islongpressed_0',['isLongPressed',['../classjsc_1_1Button.html#a6e362bebd230faea26467fdba2cd555d',1,'jsc::Button']]],
-  ['ispressed_1',['isPressed',['../classjsc_1_1Button.html#a143f0a274b03d2bede8a84e37cd43dc7',1,'jsc::Button']]]
+  ['invertinputlogic_0',['invertInputLogic',['../classjsc_1_1DigitalButton.html#a63fba8dfbe835852bb64959286008716',1,'jsc::DigitalButton']]],
+  ['isinputlogicinverted_1',['isInputLogicInverted',['../classjsc_1_1DigitalButton.html#ab8828d1cf82ce70eccae9105063dc9d6',1,'jsc::DigitalButton']]],
+  ['islongpressed_2',['isLongPressed',['../classjsc_1_1Button.html#a6e362bebd230faea26467fdba2cd555d',1,'jsc::Button']]],
+  ['ispressed_3',['isPressed',['../classjsc_1_1Button.html#a143f0a274b03d2bede8a84e37cd43dc7',1,'jsc::Button']]]
 ];
